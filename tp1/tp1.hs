30723929da97213283f6118a688ad16a1758f4af
alavia <lavia.alejandro@gmail.com>
2021-09-01 02:36:34 -0300

    Ejercicios 1,2,3,4

--- tp1/tp1.hs
+++ tp1/tp1.hs
@@ -10,35 +10,32 @@ instance Show a => Show (RTE a) where
 
 --------------Resolver--------------
 
+mismos :: Eq a => [a] -> [a] -> Bool
+mismos xs ys = foldr (\x acc -> x `elem` ys && acc) True xs && length xs == length ys
+
 instance Eq a => Eq (RTE a) where
-  (==) = undefined
+  (==) (Rose x xs) (Rose y ys) = x == y && mismos xs ys
 
+foldRose :: (a -> [(Char, b)] -> b) -> RTE a -> b
+foldRose f (Rose x hijos) = f x (map (\(e,r) -> (e, foldRose f r)) hijos)
 
-foldRose = undefined
-
-
-mapRTE = undefined
-
+mapRTE :: (a -> b) -> RTE a -> RTE b
+mapRTE f r = foldRose (\x xs -> Rose (f x) xs) r
 
 nodos :: RTE a -> [a]
-nodos = undefined
-
-
-etiquetas :: RTE a -> [Char]
-etiquetas = undefined
-
+nodos = foldRose (\x xs -> x:(foldr (++) [] (map snd xs)))
 
 altura :: RTE a -> Int
-altura = undefined
+altura = foldRose (\x xs -> if null xs then 1 else 1 + (maximum (map snd xs)))
 
+etiquetas :: RTE a -> [Char]
+etiquetas = foldRose (\x xs -> foldr (\y ys -> y++ys) [] (map (\n -> fst n : snd n) xs))
 
 ramas :: RTE a -> [String]
 ramas = undefined
 
-
 subRose :: RTE a -> Int -> RTE a
 subRose = undefined
-
 
 tests :: IO Counts
 tests = do runTestTT allTests
@@ -53,6 +50,8 @@ allTests = test [
   ]
 
 unRose = Rose 1 [('a',Rose 2 [('c',Rose 4 [])]),('b',Rose 3 [])]
+otroRose = Rose 1 [('b',Rose 3 []), ('a',Rose 2 [('c',Rose 4 [])])]
+otroRoseMas = Rose 1 [('d',Rose 5 []), ('a',Rose 2 [('c',Rose 4 [])])]
 
 testsEj1 = test [
   2 ~=? 1+1,
